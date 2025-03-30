cee10c794093a18a070503b69d7e7619d2c88520
“Mayank <mayanksrivastava11111@gmail.com>
2025-03-30 10:18:35 +0530

    final changes

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.querySelectorAll('.button1 button').forEach(button =>{button.addEventListener('click', () => {document.body.style.backgroundColor=button.textContent.toLowerCase();});});
+document.querySelectorAll('.button2 button').forEach(button =>{button.addEventListener('click', () => {document.body.style.backgroundColor=button.textContent.toLowerCase();});});
+document.querySelectorAll('.button3 button').forEach(button =>{button.addEventListener('click', () => {document.body.style.backgroundColor=button.textContent.toLowerCase();});});
+document.querySelectorAll('.button4 button').forEach(button =>{button.addEventListener('click', () => {document.body.style.backgroundColor=button.textContent.toLowerCase();});});
+document.querySelectorAll('.button5 button').forEach(button =>{button.addEventListener('click', () => {document.body.style.backgroundColor=button.textContent.toLowerCase();});});
+document.querySelectorAll('.button6 button').forEach(button =>{button.addEventListener('click', () => {document.body.style.backgroundColor=button.textContent.toLowerCase();});});
